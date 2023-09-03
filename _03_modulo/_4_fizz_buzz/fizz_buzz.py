7fed7a92ecf62beadeafbe6dd020740545d942ff
chickennuggetduck33 <luke.fun59@gmail.com>
2023-09-03 12:48:13 -0700

    bughxfdhjidgndfjbhfgjhjfgdrhgureghdjvbdfdhfdhi

--- _03_modulo/_4_fizz_buzz/fizz_buzz.py
+++ _03_modulo/_4_fizz_buzz/fizz_buzz.py
@@ -13,3 +13,10 @@
    -
    1 2 fizz 4 buzz fizz 7 8 fizz buzz 11 fizz 13 14 fizzbuzz 16 17 fizz 19 buzz
 """
+for i in range(20):
+    if i % 3 == 0:
+        print("fizz")
+    elif i % 5 == 0:
+        print("buzz")
+    else:
+        print(i)
